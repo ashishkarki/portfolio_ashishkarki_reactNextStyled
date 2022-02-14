e58c0737b28b4f82bd83a62e065106019c3e8616
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-02-14 13:57:22 +0545

    feat: :memo: took time but added images and correct links for my projects.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from 'react'
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from './ProjectsStyles'
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from '../../styles/GlobalComponents'
+import { projects } from '../../constants/constants'
+import { NEXT_APP_ELEMENT_STRINGS } from '../../constants/myConstants'
 
-const Projects = () => (
-  <div>
-    Projects
-  </div>
-);
+const Projects = () => {
+  return (
+    <Section nopadding id="projects">
+      <SectionDivider />
 
-export default Projects;
+      <SectionTitle>{NEXT_APP_ELEMENT_STRINGS.PROJECTS}</SectionTitle>
+
+      <GridContainer>
+        {projects.map((project, index) => (
+          <BlogCard key={`${project.id}-${index}`}>
+            <Img src={project.image} alt={project.title} />
+            <TitleContent>
+              <HeaderThree title>{project.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+
+            <CardInfo>Description</CardInfo>
+          </BlogCard>
+        ))}
+      </GridContainer>
+    </Section>
+  )
+}
+
+export default Projects
